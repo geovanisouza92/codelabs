b6f8c56bc0a022263b6b9e8abef45aec054e065f
Geovani de Souza <geovanisouza92@gmail.com>
2018-06-12 21:05:13 -0300

    Corrige prefixo das URLs

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -eo
 
-claat export -ga UA-120783401-1 1r0NDGM-4MiJ2TBinV_3-Pe1elRmOidIfp20PEFGGDo8
+claat export -ga UA-120783401-1 -prefix "https://geovanisouza92.github.io/codelabs/" 1r0NDGM-4MiJ2TBinV_3-Pe1elRmOidIfp20PEFGGDo8
